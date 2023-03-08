ab9d5d557807819005b0c7164143cd59d75dac2f
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-03-08 13:51:01 +0600

    all files of functional-baap-er-bank is added

---
+++ js/debug-steps.js
@@ -0,0 +1,10 @@
+/*
+Debug Steps: 
+1. Go to the live website. And try to reproduce the error.
+2. Check the console of the live website to see any error/exception. 
+3. If the error is new to you. Copy the text of the error and search online.
+4. If a line number is showing, click on that line to see the code.
+5. Add breakpoint to re run the code (refresh the website) and see whether you are getting the expected values.
+    5.1: If I am not getting the expected values, have to check which values I am not getting. If needed a breakpoint in the previous line.
+    5.2: If needed, step intu (F11) to a function
+*/
